c475b10483ce51d1a778305f00c085717bf73369
opera7133 <ainznino@pm.me>
2020-08-25 19:57:14 +0900

    first

---
+++ src/utils/siteConfig.js
@@ -0,0 +1,16 @@
+module.exports = {
+    siteUrl: `http://localhost:8000`, // Site domain. Do not include a trailing slash!
+
+    postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+
+    siteTitleMeta: `Home`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: `A starter template to build amazing static websites with Ghost and Gatsby`, // This allows an alternative site description for meta data for pages.
+
+    shareImageWidth: 1920, // Change to the width of your default share image
+    shareImageHeight: 1080, // Change to the height of your default share image
+
+    shortTitle: `Acto`, // Used for App manifest e.g. Mobile Home Screen
+    siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
+    backgroundColor: `#e9e9e9`, // Used for Offline Manifest
+    themeColor: `#15171A`, // Used for Offline Manifest
+}
